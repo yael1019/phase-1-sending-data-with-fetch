d22fab39af00248b7c2c6dc036a86c6fb28b9791
Yael Harosh <yaelharosh01@gmail.com>
2023-03-01 22:17:41 -0500

    complete

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+const submitData = (userName, userEmail) => {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    })
+        .then(resp => resp.json())
+        .then(obj => { //console.log(obj.id)
+            const body = document.getElementById('body');
+            const p = document.createElement('p');
+            body.append(p);
+            p.textContent = obj.id;
+        })
+        .catch(obj => {
+            const body = document.getElementById('body');
+            const p = document.createElement('p');
+            body.append(p);
+            p.textContent = obj.message;
+        })
+}
+
